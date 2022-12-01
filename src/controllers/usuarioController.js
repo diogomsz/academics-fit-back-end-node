630124fd06b96231ec184deecf847334bf97edd6
Diogo Martins de Assis <diogoassis3301@gmail.com>
2022-12-01 19:08:09 -0300

    commit

---
+++ src/controllers/usuarioController.js
@@ -0,0 +1,70 @@
+const database = require('../models');
+
+class UsuarioController {
+    static async pegaTodosOsUsuarios(req, res) {
+        try {
+            const todosOsUsuarios = await database.Usuarios.findAll();
+            return res.status(200).json(todosOsUsuarios);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async pegaUmUsuario(req, res) {
+        const { id } = req.params;
+        try {
+            const umUsuario = await database.Usuarios.findOne({ where: { id: Number(id) } });
+            return res.status(200).json(umUsuario);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async cadastraUsuario(req, res) {
+        const novoUsuario = req.body;
+        try {
+            const novoUsuarioCriado = await database.Usuarios.create(novoUsuario);
+            return res.status(200).json(novoUsuarioCriado);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async atualizaUsuario(req, res) {
+        const { id } = req.params;
+        const novosDados = req.body;
+        try {
+            await database.Usuarios.update(novosDados, { where: { id: Number(id) } });
+            const usuarioAtualizado = await database.Usuarios.findOne({ where: { id: Number(id) } }); 
+            return res.status(200).json(usuarioAtualizado);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async apagaUsuario(req, res) {
+        const { id } = req.params;
+        try {
+            await database.Usuarios.destroy({ where: { id: Number(id) } });
+            return res.status(200).json({ mensagem: `Usuário de id ${id} deletado` });
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async validaUsuario(req, res) {
+        const { email, senha } = req.body;
+        try {
+            const usuario = await database.Usuarios.findOne({ where: { email: email, senha: senha } });
+            if (usuario) {
+                return res.status(200).json({ mensagem: 'Usuário válido' });
+            } else {
+                return res.status(404).json({ mensagem: "Usuário não encontrado" });
+            }
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+}
+
+module.exports = UsuarioController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 const bodyParser = require("body-parser");
+const loginRouter = require('../routes/loginRoute');
 
 module.exports = app => {
     app.get('/', (req, res) => {
         res.status(200).json({ message: 'back-end diogo martins' });
     });
-    app.use(bodyParser.json());
+    app.use(bodyParser.json(), loginRouter);
 };
